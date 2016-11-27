a4a360f3b927e2642d39db226f0636bdc203aba9
Rob Moore <giftiger.wunsch@xantoria.com>
2016-11-27 17:46:13 +0000

    Add report skeleton endpoint

--- src/main/scala/com/xantoria/auditorium/api/EventReporting.scala
+++ src/main/scala/com/xantoria/auditorium/api/EventReporting.scala
@@ -6,21 +6,44 @@ import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import spray.json.DefaultJsonProtocol._
 
-case class Pong(
-  success: Boolean = true,
-  message: Option[String] = None
-)
+import com.xantoria.auditorium.reporting._
 
 trait EventReporting {
-  implicit val pongFormat = jsonFormat2(Pong)
+  import EventReporting._
 
-  private val pingRoute: Route = path("ping" / Segment / Slash.?) {
-    s: String => get {
-      complete {
-        Pong(message = Some(s"Echoed: $s"))
+  implicit val reportFormat = jsonFormat(
+    Report.apply,
+    "id", "timestamp", "reporter", "event_type", "object_id", "summary", "user"
+  )
+  implicit val ackFormat = jsonFormat2(Ack.apply)
+
+  private val reportEvent: Route = path("report" ~ Slash.?) {
+    post {
+      entity(as[Report]) {
+        report: Report => {
+          val resp: (Int, Ack) = {
+            val errors: List[String] = report.configErrors
+
+            if (errors.isEmpty) {
+              202 -> Ack.success
+            } else {
+              400 -> Ack.error(s"Validation errors: ${errors.mkString(", ")}")
+            }
+          }
+          complete(resp)
+        }
       }
     }
   }
 
-  protected val eventRoutes: Route = pingRoute
+  protected val eventRoutes: Route = reportEvent
+}
+
+object EventReporting {
+  case class Ack(success: Boolean, error: Option[String])
+
+  object Ack {
+    def success: Ack = Ack(success = true, error = None)
+    def error(msg: String): Ack = Ack(success = false, error = Some(msg))
+  }
 }
---
+++ src/main/scala/com/xantoria/auditorium/reporting/Report.scala
@@ -0,0 +1,56 @@
+package com.xantoria.auditorium.reporting
+
+import scala.util.matching.Regex
+
+/**
+ * Represents a report of an action to be logged
+ *
+ * @param timestamp When the event happened, as a UTC ISO 8601 string like 2000-01-01T00:00:00Z
+ * @param reporter An identifier for the service reporting the event
+ * @param eventType The type of event which occurred; possible options are configurable
+ * @param objectId The ID of the object on which the action was taken. This is assumed to uniquely
+ *                 identify a resource in a manner which all subscribers will understand (or at
+ *                 at least know whether to ignore if it's not an identifier they care about)
+ * @param summary A text summary of the action which was taken, intended as a human-readable
+ *                description.
+ * @param user If this action can be attributed to a particular user, provide that user's ID
+ */
+case class Report(
+  id: String,
+  timestamp: String,
+  reporter: String,
+  eventType: String,
+  objectId: String,
+  summary: String,
+  user: Option[String]
+) {
+  /**
+   * A human-readable list of reasons this Report does not pass validation
+   */
+  lazy val configErrors: List[String] = {
+    val checks: List[(Boolean, String)] = List(
+      (
+        timestamp match {
+          case Report.timestampPattern() => true
+          case _ => false
+        },
+        "Bad timestamp pattern"
+      ),
+      // TODO: Make this one of a configurable list of options (typesafe config)
+      List("CREATE", "DELETE", "EDIT", "OTHER").contains(eventType) -> "Unsupported event type",
+      (id.length <= Report.maxFieldLength) -> "Bad length: `id`",
+      (objectId.length <= Report.maxFieldLength) -> "Bad length: `object_id`",
+      (summary.length <= Report.maxFieldLength) -> "Bad length: `summary`",
+      (user.map { _.length <= Report.maxFieldLength } getOrElse true) -> "Bad length: `user`"
+    )
+
+    checks flatMap {
+      case (passed: Boolean, errorMsg: String) => if (passed) None else Some(errorMsg)
+    }
+  }
+}
+
+object Report {
+  val timestampPattern: Regex = """^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z$""".r
+  val maxFieldLength: Int = 512 // Fairly generous general limit, mostly to allow long summaries
+}
